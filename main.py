de3f302b197041b565a327c54e34b2bba49e658b
suarez605 <alvaro.suarez2212@gmail.com>
2022-06-07 16:49:29 +0200

    finished

--- main.py
+++ main.py
@@ -12,18 +12,7 @@ from settings import settings
 
 logger = logging.getLogger()
 
-# "https://europe-west1-kanaraflutter-7a8f7.cloudfunctions.net/normalizationAPI-º" PROD
-
-#client = pymongo.MongoClient("mongodb+srv://VectorizationAPI:WOxaSL1b6widgU7Q@kanaratest.y1kx1.mongodb.net/testretryWrites=true&w=majority") # TESTS
-#client = pymongo.MongoClient("mongodb+srv://VectorizationAPI:yF6gfh2n6VALktyk@dev.inl0u.mongodb.net/Kanara?retryWrites=true&w=majority") #DEV
-# "mongodb+srv://VectorizationAPI:8oq34nZ5dePNBYXM@kanaraprod.mjzzm.mongodb.net/Kanara?retryWrites=true&w=majority" PROD
-
-
 # STORAGE
-import google.auth.credentials as creds
-
-a = creds.Credentials()
-
 storage_client = storage.Client()
 bucket = storage_client.bucket(settings.FITS_BUCKET)
 
@@ -50,15 +39,16 @@ def process_sess(sess):
     time.sleep(1)
     res = requests.post(url=settings.NORMALIZATION_URL, data=fit, params=params_dic, headers={'Content-Type': 'application/octet-stream'})
     logger.info(res.status_code)
+    print(res.status_code)
     if res.status_code != 200:
         logger.error(f"Error with session: {id_mongo} on normalization/clean")
         logger.error(res.content)
         return
 
     params_back = {
-        "user_id": sess["firestore_user_id"],
-        "session_id": id_mongo,
-        "sport": "" if sport_id is None else sport_id,
+        "userId": sess["firestore_user_id"],
+        "sessionId": id_mongo,
+        "sportId": "" if sport_id is None else sport_id,
         "privacy": "public" if privacy is None else privacy
     }
 
@@ -76,7 +66,7 @@ def get_fit(fit_name):
 
 threads = []
 with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-    for skip in range(0, 50, 5):
+    for skip in range(0, 1, 5):
         client = pymongo.MongoClient(settings.MONGO_CONNECTION, tlsCAFile=certifi.where()) #PROD
         db_mongo = client.Kanara
         print(f"Number of elements: {skip}")
